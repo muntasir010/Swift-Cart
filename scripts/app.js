56f43e086d7061c0390228335083e8ca326be94a
muntasir010 <naeemtasir03@gmail.com>
2026-02-16 20:47:58 +0600

    create simple hero section

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
-function includeHTML() {
+function loadSections() {
   const navbarDiv = document.getElementById("navbar");
-
   if (navbarDiv) {
     fetch("components/navbar.html")
       .then((response) => {
@@ -12,6 +11,19 @@ function includeHTML() {
       })
       .catch((error) => console.error(error));
   }
+
+  const heroDiv = document.getElementById("hero");
+  if (heroDiv) {
+    fetch("components/hero.html")
+      .then((response) => {
+        if (response.ok) return response.text();
+        throw new Error("Hero section file not found");
+      })
+      .then((data) => {
+        heroDiv.innerHTML = data;
+      })
+      .catch((error) => console.error(error));
+  }
 }
 
-window.onload = includeHTML;
+window.onload = loadSections;
